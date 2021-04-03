200d18af15b38a96871a078c0a6006e093151451
Lemish891 <2469780M@student.gla.ac.uk>
2021-04-03 21:35:05 +0100

    Update settings.py

--- rate_my_meme_coursework/settings.py
+++ rate_my_meme_coursework/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
 
 LOGIN_URL = '/rango/login/'
 
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'rate_my_meme_coursework.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ]
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
