dea7294d84c12bda3f42d3b656d867b53a6e29f1
Lemish891 <2469780M@student.gla.ac.uk>
2021-04-03 15:34:35 +0100

    base modules

--- rango/models.py
+++ rango/models.py
@@ -10,3 +10,26 @@ class UserProfile(models.Model):
    
     def __str__(self):
         return self.user.username
+   
+   
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    slug = models.SlugField()
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+    
+    class Meta:
+        verbose_name_plural = 'categories'
+    
+    def __str__(self):
+        return self.name
+
+
+class Meme(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='memes')
+    
+    def __str__(self):
+        return self.image
