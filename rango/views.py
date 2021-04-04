d490fded71ebda23033e37793d3dc20a1df5b1ab
Lemish891 <2469780M@student.gla.ac.uk>
2021-04-04 18:01:45 +0100

    cookies

--- rango/views.py
+++ rango/views.py
@@ -8,6 +8,7 @@ from django.http import HttpResponse
 from django.urls import reverse
 from django.shortcuts import redirect, render
 from rango.models import Category, Meme
+from datetime import datetime
 
 def index(request):
     category_list = Category.objects.order_by('-likes')
@@ -17,7 +18,12 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['memes'] = meme_list
 
-    return render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    response =  render(request, 'rango/index.html', context=context_dict)
+    return response
+
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -30,6 +36,7 @@ def show_category(request, category_name_slug):
     except Category.DoesNotExist:
         context_dict['category'] = None
         context_dict['memes'] = None
+
     return render(request, 'rango/category.html', context=context_dict)
 
 def add_meme(request, category_name_slug):
@@ -123,3 +130,24 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request):
+    visits = int(request.COOKIES.get('visits', '1'))
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
