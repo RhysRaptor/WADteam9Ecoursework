130b8c9e044557e19d1c56df28f6892bc1c31d9c
FarajMonnapillai <77503528+FarajMonnapillai@users.noreply.github.com>
2021-04-05 19:06:09 +0100

    fixs

--- rango/views.py
+++ rango/views.py
@@ -92,8 +92,6 @@ def register(request):
             profile = profile_form.save(commit=False)
             profile.user = user
             
-            if 'picture' in request.FILES:
-                profile.picture = request.FILES['picture']
                 
             profile.save()
             registered = True
