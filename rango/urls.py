cc93c8550cf779e3a9adc607aaa001add3220e51
FarajMonnapillai <77503528+FarajMonnapillai@users.noreply.github.com>
2021-04-05 18:39:19 +0100

    login

--- rango/urls.py
+++ rango/urls.py
@@ -10,8 +10,7 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('restricted/', views.restricted, name='restricted'),
     path('logout/', views.user_logout, name='logout'),
-    path('category/<slug:category_name_slug>/',
-		  views.show_category, name='show_category'),
+    path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
     path('category/<slug:category_name_slug>/upload/', views.add_meme, name='upload'),
-    path('allmemes/<slug:meme_title_slug>/', views.allmemes, name='allmemes')
+    path('allmemes/<slug:meme_title_slug>/', views.allmemes, name='allmemes'),
 ]
